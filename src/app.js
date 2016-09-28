f5ddaf8eb3392d6cd67f1727eafdb208acd3d5d0
kelvinabrokwa <kelvinabrokwa@gmail.com>
2016-09-28 15:51:39 -0400

    parse and display metadata

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import jsmpeg from 'jsmpeg';
 
-const HOST = 'makerspace-video.herokuapp.com'
+// TODO: throw these into .env
+const HOST = 'makerspace-video.herokuapp.com';
+const ENDPOINT = 'video';
+
 /**
- * return all elements in b that are not in a
+ * return an array of all elements in b that arent's in a
  */
 function diff(a, b) {
   let c = [];
@@ -17,26 +20,47 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      streams: []
+      streams: {}
     };
     this.players = {};
   }
 
   componentDidMount() {
-    fetch(`https://${HOST}/video/list`)
+    fetch(`https://${HOST}/${ENDPOINT}/list`)
       .then(res => res.json())
       .then(streams => {
-        this.setState({ streams });
+        this.setState({
+          streams: streams.reduce((p, c) => {
+            p[c] = { metadata: {} }
+            return p;
+          }, {})
+        });
       });
   }
 
   componentDidUpdate(prevProps, prevState) {
+    // we wait to connect sockets to new steams in order to ensure that all
+    // canvas elements needed have been mounted
+
     const prevStreams = prevState.streams;
     const { streams } = this.state;
-    const newStreams = diff(prevStreams, streams);
-    let client;
+    const newStreams = diff(Object.keys(prevStreams), Object.keys(streams));
+
+    // fetch metadata for new streams
     for (let i = 0; i < newStreams.length; i++) {
-      client = new WebSocket(`wss://${HOST}/video/out/${newStreams[i]}`);
+      fetch(`https://${HOST}/${ENDPOINT}/out/${newStreams[i]}/metadata`)
+        .then(res => res.json())
+        .then(metadata => {
+          streams[newStreams[i]].metadata = metadata;
+          this.setState({ streams });
+        });
+    }
+
+    // create sockets to new streams
+    let client;
+
+    for (let i = 0; i < newStreams.length; i++) {
+      client = new WebSocket(`wss://${HOST}/${ENDPOINT}/out/${newStreams[i]}`);
       this.players[newStreams[i]] = {
         client,
         player: new jsmpeg(client, { canvas: this.refs[`video${newStreams[i]}`] })
@@ -47,7 +71,14 @@ class App extends React.Component {
   render() {
     const { streams } = this.state;
     return (<div>
-      {streams.map(stream => <canvas ref={`video${stream}`} key={stream}></canvas>)}
+      {Object.keys(streams).map(stream => <div key={stream}>
+        <canvas ref={`video${stream}`}></canvas>
+        <div>
+          {Object.keys(streams[stream].metadata).map((key, i) => <div key={i}>
+            {key}: {streams[stream].metadata[key]}
+          </div>)}
+        </div>
+      </div>)}
     </div>);
   }
 
